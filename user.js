13d8f8e90d850f8eef44bd7c3a8210eb69ae2187
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-03-08 12:31:12 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,26 +1,25 @@
 // ==UserScript==
 // @name         51CTO不绑定微信
 // @namespace    https://qinlili.bid
-// @version      0.1
+// @version      0.2
 // @description  无需绑定微信就能看免费课
 // @author       琴梨梨
 // @match        https://edu.51cto.com/course/*.html
-// @icon        https://edu.51cto.com/favicon.ico
+// @icon         https://edu.51cto.com/favicon.ico
 // @grant        none
+// @license      WTFPL
 // ==/UserScript==
 
 (function() {
     'use strict';
-
     (function(open) {
 
-    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
+        oldXML.prototype.open = function(method, url, async, user, pass) {
+            if(url.indexOf("wechat-status")>0){
+                url= "data:application/json;base64,eyJzdGF0dXMiOjEsIm1zZyI6IiIsImRhdGEiOjExNDUxNH0=";
+            }
+            open.call(this, method, url, async, user, pass);
+        };
 
-if(url.indexOf("wechat-status")>0){
-    url= "data:application/json;base64,eyJzdGF0dXMiOjEsIm1zZyI6IiIsImRhdGEiOjExNDUxNH0=";
-               }
-        open.call(this, method, url, async, user, pass);
-    };
-
-})(XMLHttpRequest.prototype.open);
+    })(oldXML.prototype.open);
 })();
